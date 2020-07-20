5086c7aaa2f0323374ec471ece18a171ac7c1913
Doris <hpan996@gmail.com>
2020-07-20 15:27:09 -0700

    view model prac completed, 43 finished

--- app/src/main/java/com/laioffer/tinnews/network/RetrofitClient.java
+++ app/src/main/java/com/laioffer/tinnews/network/RetrofitClient.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.Log;
 
 import com.ashokvarma.gander.GanderInterceptor;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 
 import java.io.IOException;
 
@@ -24,6 +25,7 @@ public class RetrofitClient {
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .addInterceptor(new HeaderInterceptor())
                 .addInterceptor(new GanderInterceptor(context).showNotification(true))
+                .addNetworkInterceptor(new StethoInterceptor())
                 .build();
         return new Retrofit.Builder()
                 .baseUrl(BASE_URL)
